ff715a39f87f403cfc4a2a2a39343d1541163fa8
Candy51 <31677753+Candy51@users.noreply.github.com>
2017-09-13 18:53:57 +0900

    Add files via upload
    Professor Jeong!
    I feel really sorry that i uploaded my 1 homework.
    I'll try really hard from now on!

---
+++ 1hw.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  printf("Student Id Number: 2017301051\n");
+  printf("Name: Lee Kang Eun\n");
+  printf("Mobile Phone Number: 010-8636-3039\n");
+  printf("My git hub adress for Programming Practice lecture:https://github.com/Candy51/C_1st_Assignment-\n");
+  return 0;
+}
